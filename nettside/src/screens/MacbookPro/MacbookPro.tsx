a71305067a6227f18f3e33aeb6e3909afdab835e
Jørgen Sandhaug <jorgen.sandhaug@gmail.com>
2024-02-12 10:08:07 +0100

    initial

---
+++ nettside/src/screens/MacbookPro/MacbookPro.tsx
@@ -0,0 +1,174 @@
+import React from "react";
+import { Group } from "../../components/Group";
+
+export const MacbookPro = (): JSX.Element => {
+  return (
+    <div className="bg-white flex flex-row justify-center w-full">
+      <div className="bg-white overflow-hidden w-[1728px] h-[3117px] relative">
+        <div className="absolute w-[1728px] h-[1117px] top-0 left-0">
+          <div className="h-[89px] top-[37px] bg-[#d9d9d9] absolute w-[1728px] left-0" />
+          <img
+            className="absolute w-[1728px] h-[1117px] top-0 left-0"
+            alt="Mask group"
+            src="https://c.animaapp.com/Gk9yvO1z/img/mask-group-4.png" // bakgrunn (29, 44, 72)
+          />
+          <img
+            className="absolute w-[864px] h-[952px] top-0 left-[864px]"
+            alt="Mask group"
+            src="https://c.animaapp.com/Gk9yvO1z/img/mask-group-5.png" //inif-bilde main 
+          />
+          <div className="flex flex-col w-[1373px] h-[609px] items-start gap-[25px] absolute top-[308px] left-[73px]">
+            <p className="relative w-[835px] h-[296px] mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-white text-[82px] tracking-[-0.47px] leading-[normal]">
+              Kompromissløs avkastning for en verre verden.
+            </p>
+            <p className="relative w-[629px] h-[97px] [font-family:'Inter',Helvetica] font-normal text-white text-[24px] tracking-[-0.14px] leading-[normal]">
+              Børsklubben for finansinteresserte studenter ved Industriell økonomi og teknologiledelse (indøk) ved NTNU,
+              Trondheim.
+            </p>
+            <div className="inline-flex items-start gap-[25px] relative flex-[0_0_auto]">
+              <div className="inline-flex items-center justify-center gap-[10px] px-[50px] py-[15px] relative flex-[0_0_auto] bg-white rounded-[10px]">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-[#182c49] text-[24px] tracking-[-0.14px] leading-[normal]">
+                  Årsrapport
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-center gap-[10px] px-[50px] py-[15px] relative flex-[0_0_auto] rounded-[10px] border border-solid border-white">
+                <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-white text-[24px] tracking-[-0.14px] leading-[normal]">
+                  Om oss
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="absolute w-[1542px] h-[60px] top-[34px] left-[73px]">
+            <div className="top-[15px] left-[275px] text-white absolute [font-family:'Inter',Helvetica] font-normal text-[24px] tracking-[-0.14px] leading-[normal]">
+              Om oss
+            </div>
+            <div className="absolute top-[15px] left-[430px] [font-family:'Inter',Helvetica] font-normal text-white text-[24px] tracking-[-0.14px] leading-[normal]">
+              Veldedighetsfond
+            </div>
+            <div className="absolute top-[15px] left-[698px] [font-family:'Inter',Helvetica] font-normal text-white text-[24px] tracking-[-0.14px] leading-[normal]">
+              Blogg
+            </div>
+            <img
+              className="absolute w-[161px] h-[60px] top-0 left-0"
+              alt="Vector"
+              src="https://c.animaapp.com/Gk9yvO1z/img/vector.svg" // white background
+            />
+            <div className="inline-flex items-center justify-center gap-[10px] px-[36px] py-[11px] absolute top-[4px] left-[1085px] bg-white rounded-[10px]">
+              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-[#182c49] text-[24px] tracking-[-0.14px] leading-[normal]">
+                Publikasjoner
+              </div>
+            </div>
+            <div className="inline-flex items-center justify-center gap-[10px] px-[36px] py-[11px] absolute top-[4px] left-[1323px] rounded-[10px] border border-solid border-white">
+              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-white text-[24px] tracking-[-0.14px] leading-[normal]">
+                For bedrifter
+              </div>
+            </div>
+          </div>
+          <div className="absolute w-[430px] h-[45px] top-[871px] left-[1181px]">
+            <div className="absolute w-[359px] top-[5px] left-0 [font-family:'Inter',Helvetica] font-normal text-white text-[30px] text-right tracking-[0] leading-[normal] whitespace-nowrap">
+              @indokfinans
+            </div>
+            <img
+              className="absolute w-[42px] h-[45px] top-0 left-[385px]"
+              alt="Instagram seeklogo"
+              src="https://c.animaapp.com/Gk9yvO1z/img/instagram-seeklogo-com-1.svg"// instagram icon
+            />
+          </div>
+          <div className="absolute w-[702px] h-[45px] top-[987px] left-[909px]">
+            <div className="absolute w-[635px] top-[5px] left-0 [font-family:'Inter',Helvetica] font-normal text-white text-[30px] text-right tracking-[0] leading-[normal] whitespace-nowrap">
+              InFi - Indøk Finansklubb
+            </div>
+            <img
+              className="absolute w-[42px] h-[45px] top-0 left-[657px]"
+              alt="Facebook icon"
+              src="https://c.animaapp.com/Gk9yvO1z/img/facebook-icon-2013-1.svg" // facebook icon
+            />
+          </div>
+          <div className="absolute w-[702px] h-[45px] top-[928px] left-[908px]">
+            <div className="absolute w-[635px] top-[5px] left-0 [font-family:'Inter',Helvetica] font-normal text-white text-[30px] text-right tracking-[0] leading-[normal] whitespace-nowrap">
+              InFi - Indøk Finansklubb
+            </div>
+            <img
+              className="absolute w-[42px] h-[45px] top-0 left-[657px]"
+              alt="Linkedin icon"
+              src="https://c.animaapp.com/Gk9yvO1z/img/linkedin-icon-1.svg" // linkedin icon
+            />
+          </div>
+          <img
+            className="absolute w-[106px] h-[113px] top-[881px] left-[908px]"
+            alt="Group"
+            src="https://c.animaapp.com/Gk9yvO1z/img/group-5@2x.png"
+          />
+          <img
+            className="absolute w-[145px] h-[212px] top-[421px] left-[1583px]"
+            alt="Group"
+            src="https://c.animaapp.com/Gk9yvO1z/img/group-17@2x.png"
+          />
+          <img
+            className="absolute w-[29px] h-[113px] top-[321px] left-0"
+            alt="Group"
+            src="https://c.animaapp.com/Gk9yvO1z/img/group-5-1@2x.png"
+          />
+          <img
+            className="absolute w-[327px] h-[167px] top-[927px] left-[48px]"
+            alt="Group"
+            src="https://c.animaapp.com/Gk9yvO1z/img/group-10@2x.png" //carnegie and dnb
+          />
+        </div>
+        <div className="inline-flex items-center gap-[81px] absolute top-[1117px] left-[73px]">
+          <div className="relative w-[841px] h-[395px]">
+            <p className="absolute w-[812px] top-[158px] left-[6px] [font-family:'Inter',Helvetica] font-normal text-black text-[24px] tracking-[-0.14px] leading-[normal]">
+              Indøk Finansklubb ble etablert våren 2021 for å samle finansinteresserte studenter ved Industriell økonomi
+              og teknologiledelse (indøk) ved NTNU, Trondheim. Finansklubben består av studenter fra alle årstrinn, med
+              hensikt å fasilitere for kunnskaps- og erfaringsoverføring på tvers av kullene. Det første året etter
+              oppstart forvaltet finansklubben en papirportefølje som utkonkurrerte Oslo Børs.
+              <br />
+              <br />
+              Les mer &gt;
+            </p>
+            <div className="absolute w-[835px] top-[57px] left-0 [font-family:'Inter',Helvetica] font-normal text-black text-[60px] tracking-[-0.34px] leading-[normal]">
+              Tidlig eksponering
+            </div>
+            <div className="top-0 left-[6px] text-[#ff3131] absolute [font-family:'Inter',Helvetica] font-normal text-[24px] tracking-[-0.14px] leading-[normal]">
+              OM OSS
+            </div>
+          </div>
+          <img
+            className="relative w-[739px] h-[630px]"
+            alt="Mask group"
+            src="https://c.animaapp.com/Gk9yvO1z/img/mask-group-6.png" // image with multiple people
+          />
+        </div>
+        <div className="absolute w-[1775px] h-[1032px] top-[1747px] left-0">
+          <div className="h-[1032px] top-0 bg-[#f2f2f2] absolute w-[1728px] left-0" />
+          <div className="inline-flex flex-col items-start gap-[28px] absolute top-[55px] left-[73px]">
+            <Group />
+            <div className="relative w-fit [font-family:'Inter',Helvetica] font-normal text-black text-[30px] tracking-[-0.17px] leading-[normal] whitespace-nowrap">
+              Kategorier:
+            </div>
+            <div className="inline-flex items-center justify-center gap-[10px] px-[40px] py-[9px] relative flex-[0_0_auto] bg-[#1f3553] rounded-[10px] border border-solid">
+              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-white text-[24px] tracking-[-0.14px] leading-[normal]">
+                Nyheter
+              </div>
+            </div>
+          </div>
+          <div className="inline-flex items-center justify-center gap-[10px] px-[40px] py-[9px] absolute top-[920px] left-[261px] rounded-[10px] border border-solid border-black">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-black text-[24px] tracking-[-0.14px] leading-[normal]">
+              Publikasjoner
+            </div>
+          </div>
+          <div className="inline-flex items-center justify-center gap-[10px] px-[40px] py-[9px] absolute top-[920px] left-[501px] rounded-[10px] border border-solid border-black">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-black text-[24px] tracking-[-0.14px] leading-[normal]">
+              Eventer
+            </div>
+          </div>
+          <div className="inline-flex items-center justify-center gap-[10px] px-[40px] py-[9px] absolute top-[920px] left-[677px] rounded-[10px] border border-solid border-black">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-black text-[24px] tracking-[-0.14px] leading-[normal]">
+              Annet
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
